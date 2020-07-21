bd03453302f34523ae8d3c0d32b598e8969b68b2
jrwnter <robin.winter@bayer.com>
2020-07-21 10:19:10 +0200

    run 29

--- graphae/trainer.py
+++ graphae/trainer.py
@@ -50,7 +50,7 @@ class PLGraphAE(pl.LightningModule):
 
     def configure_optimizers(self):
         opt_enc = torch.optim.Adam(self.graph_ae.encoder.parameters(), lr=0.00002, betas=(0.5, 0.99))
-        opt_dec = torch.optim.Adam(self.graph_ae.decoder.parameters(), lr=0.00002, betas=(0.5, 0.99))
+        opt_dec = torch.optim.Adam(self.graph_ae.decoder.parameters(), lr=0.00005, betas=(0.5, 0.99))
         lr_scheduler = torch.optim.lr_scheduler.StepLR(
             optimizer=opt_enc,
             step_size=2,
