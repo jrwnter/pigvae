7368f8872851a43b78beacf207230c3b46c4dbd8
jrwnter <robin.winter@bayer.com>
2020-07-23 17:53:11 +0200

    working run 60

--- graphae/graph_ae.py
+++ graphae/graph_ae.py
@@ -77,12 +77,20 @@ class GraphAE(torch.nn.Module):
         super().__init__()
         self.encoder = Encoder(hparams)
         self.decoder = Decoder(hparams)
-        self.predictor = SideTaskPredictor(hparams)
+        z_dim = hparams["emb_dim"]
+        self.fc_mu = torch.nn.Linear(z_dim, z_dim)
+        self.fc_logvar = torch.nn.Linear(z_dim, z_dim)
 
-    def forward(self, node_features, adj, mask, grad_mode="encode"):
+    def forward(self, node_features, adj, mask):
         mol_emb = self.encoder(node_features, adj, mask)
-        node_features_, adj_, mask_ = self.decoder(mol_emb.detach())
+        mu = self.fc_mu(mol_emb)
+        logvar = self.fc_logvar(mol_emb)
+        mol_emb = reparameterize(mu, logvar)
+        node_features_, adj_, mask_ = self.decoder(mol_emb)
         mol_emb_ = self.encoder(node_features_, adj_, mask_)
+        mu_ = self.fc_mu(mol_emb_)
+        logvar_ = self.fc_logvar(mol_emb_)
+        mol_emb_ = reparameterize(mu_, logvar_)
         output = {
             "node_features_real": node_features,
             "node_features_pred": node_features_,
@@ -92,7 +100,18 @@ class GraphAE(torch.nn.Module):
             "mask_pred": mask_,
             "mol_emb_real": mol_emb,
             "mol_emb_pred": mol_emb_,
+            "mu_real": mu,
+            "mu_pred": mu_,
+            "logvar_real": logvar,
+            "logvar_pred": logvar_,
         }
 
         return output
+
+
+def reparameterize(mu, logvar):
+    std = torch.exp(0.5 * logvar)
+    eps = torch.randn_like(std)
+    return mu + eps * std
+
 
--- graphae/train.py
+++ graphae/train.py
@@ -1,10 +1,11 @@
 import numpy as np
+import os
 from argparse import ArgumentParser
 import torch
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 import pandas as pd
-from graphae.graph_ae import GraphAE
+from graphae.graph_ae import GraphAE, reparameterize
 from graphae.data import MolecularGraphDataset, add_noise
 from graphae.loss import critic, resemblance_loss, kld_loss
 from graphae.hyperparameter import add_arguments
@@ -12,6 +13,11 @@ from torch.nn.functional import mse_loss, triplet_margin_loss
 
 
 def train(hparams):
+    save_dir = hparams["save_dir"] + "/run{}/".format(hparams["id"])
+    save_file = os.path.join(save_dir, "save.ckpt")
+    if not os.path.isdir(save_dir):
+        print("Creating directory")
+        os.mkdir(save_dir)
     graphs = np.load("1000000_16mnn_graphs.npy")
     train_dataset = MolecularGraphDataset(graphs=graphs[hparams["num_eval_samples"]:], noise=True)
     eval_dataset = MolecularGraphDataset(graphs=graphs[:hparams["num_eval_samples"]], noise=False)
@@ -29,33 +35,97 @@ def train(hparams):
         shuffle=False,
         pin_memory=True
     )
-    device = "cuda:{}".fomrat(hparams["gpus"])
+    device = "cuda:{}".format(hparams["gpus"])
 
-    model = GraphAE(hparams)
+    model = GraphAE(hparams).to(device)
 
-    opt_enc = torch.optim.Adam(list(model.encoder.parameters()) + list(model.predictor.parameters()),
-                               lr=0.0001)
-    opt_dec = torch.optim.Adam(model.decoder.parameters(), lr=0.0001)
+    opt_enc = torch.optim.Adam(model.encoder.parameters(), lr=0.00002, betas=(0.5, 0.99))
+    opt_dec = torch.optim.Adam(model.decoder.parameters(), lr=0.0001, betas=(0.5, 0.99))
 
+    step = 0
+    log = {"dec_loss": [], "enc_loss": [], "kld_loss_real": [], "kld_loss_pred": [], "sparsity_loss": []}
     for epoch in range(50):
+        print("Epoch: ", epoch)
         for batch in train_dataloader:
-            node_features, adj, mask = batch[0].to(device)
-            noisy_node_features, noisy_adj, noisy_mask = add_noise(
-                node_features, adj, mask, std=0.2 * (0.9 ** epoch))
-
+            step += 1
+            node_features, adj, mask = batch[0].to(device), batch[1].to(device), batch[2].to(device)
             opt_enc.zero_grad()
 
             mol_emb = model.encoder(node_features, adj, mask)
+            mu = model.fc_mu(mol_emb)
+            logvar = model.fc_logvar(mol_emb)
+            mol_emb = reparameterize(mu, logvar)
             with torch.no_grad():
                 node_features_pred, adj_pred, mask_pred = model.decoder(mol_emb)
             mol_emb_pred = model.encoder(node_features_pred, adj_pred, mask_pred)
-            noisy_mol_emb_real = model.encoder(noisy_node_features, noisy_adj, noisy_mask)
-            enc_loss = triplet_margin_loss(
-                anchor=mol_emb,
-                positive=noisy_mol_emb_real,
-                negative=mol_emb_pred,
-                margin=0.5
+            mu_pred = model.fc_mu(mol_emb_pred)
+            logvar_pred = model.fc_logvar(mol_emb_pred)
+            mol_emb_pred = reparameterize(mu_pred, logvar_pred)
+            enc_loss = -1 * mse_loss(
+                input=mol_emb_pred,
+                target=mol_emb
             )
+            kld_loss_real = 0.1 * kld_loss(mu, logvar)
+            kld_loss_pred = 0.1 * kld_loss(mu_pred, logvar_pred)
+
+            loss = enc_loss + kld_loss_real + kld_loss_pred
+
+            loss.backward()
+            opt_enc.step()
+
+
+            opt_dec.zero_grad()
+            with torch.no_grad():
+                mol_emb = model.encoder(node_features, adj, mask)
+            node_features_pred, adj_pred, mask_pred = model.decoder(mol_emb)
+            mol_emb_pred = model.encoder(node_features_pred, adj_pred, mask_pred)
+            mu_pred = model.fc_mu(mol_emb_pred)
+            logvar_pred = model.fc_logvar(mol_emb_pred)
+            mol_emb_pred = reparameterize(mu_pred, logvar_pred)
+            dec_loss = mse_loss(
+                input=mol_emb_pred,
+                target=mol_emb
+            )
+            sparsity_loss = 0.1 * torch.min(torch.abs(mask_pred - torch.ones_like(mask_pred)),
+                                            torch.abs(mask_pred - torch.zeros_like(mask_pred))).mean()
+            sparsity_loss += 0.1 * torch.min(torch.abs(adj_pred - torch.ones_like(adj_pred)),
+                                             torch.abs(adj_pred - torch.zeros_like(adj_pred))).mean()
+            loss = dec_loss + sparsity_loss
+
+
+
+            loss.backward()
+            opt_dec.step()
+
+            log["enc_loss"].append(enc_loss.item())
+            log["dec_loss"].append(dec_loss.item())
+            log["kld_loss_real"].append(kld_loss_real.item())
+            log["kld_loss_pred"].append(kld_loss_pred.item())
+            log["sparsity_loss"].append(sparsity_loss.item())
+
+            if step % 200 == 0:
+                metrics = []
+                for batch in eval_dataloader:
+                    node_features, adj, mask = batch[0].to(device), batch[1].to(device), batch[2].to(device)
+                    output = model(node_features, adj, mask)
+                    metrics.append(critic(
+                        mol_emb=output["mol_emb_real"],
+                        mol_emb_gen=output["mol_emb_pred"],
+                        mask=output["mask_real"],
+                        mask_gen=output["mask_pred"],
+                        adj=output["adj_real"],
+                        adj_gen=output["adj_pred"],
+                    ))
+                out = {}
+                for key in metrics[0].keys():
+                    out[key] = torch.stack([output[key] for output in metrics]).mean().item()
+                for key, value in log.items():
+                    log[key] = np.mean(log[key])
+                print({**log, **out})
+                torch.save(model, save_file)
+                log = {"dec_loss": [], "enc_loss": [], "kld_loss_real": [], "kld_loss_pred": [], "sparsity_loss": []}
+
+        return output
 
 
 if __name__ == '__main__':
