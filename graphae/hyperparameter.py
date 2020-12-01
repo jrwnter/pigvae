89f0ae48a064ae630d67d7b2e9fbe368df69000d
jrwnter <robin.winter@bayer.com>
2020-12-01 18:04:13 +0100

    again max train samples? error otherwise?

--- graphae/hyperparameter.py
+++ graphae/hyperparameter.py
@@ -18,7 +18,7 @@ def add_arguments(parser):
     parser.add_argument('-g', '--gpus', default=1, type=int)
     parser.add_argument('-e', '--num_epochs', default=5000, type=int)
     parser.add_argument("--num_eval_samples", default=50000, type=int)
-    parser.add_argument("--num_samples_per_epoch", default=4000000, type=int)
+    parser.add_argument("--num_samples_per_epoch", default=800000000, type=int)
     parser.add_argument("--num_samples_per_epoch_inc", default=2000000, type=int)
     parser.add_argument("--eval_freq", default=500, type=int)
     parser.add_argument("-s", "--save_dir", default=DEFAULT_SAVE_DIR, type=str)
