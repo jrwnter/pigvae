9cddaf8c681d23082af13348ae3e5a83094c2581
jrwnter <robin.winter@bayer.com>
2020-12-02 14:21:11 +0100

    lr decay

--- graphae/trainer.py
+++ graphae/trainer.py
@@ -24,7 +24,7 @@ class PLGraphAE(pl.LightningModule):
         self.tau_scheduler = TauScheduler(
             start_value=hparams["tau"],
             factor=0.95,
-            step_size=5
+            step_size=1
         )
 
     def forward(self, graph, training, tau, postprocess_method=None):
@@ -121,14 +121,18 @@ class PLGraphAE(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.graph_ae.parameters(), lr=self.hparams["lr"])
-        lr_scheduler = torch.optim.lr_scheduler.StepLR(
+        lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
             optimizer=optimizer,
-            step_size=10,
-            gamma=0.5
+            factor=0.5,
+            patience=20,
+            cooldown=50,
+            min_lr=1e-6,
         )
         scheduler = {
             'scheduler': lr_scheduler,
-            'interval': 'epoch',
+            'interval': 'step',
+            'monitor': 'val_hard_loss',
+            'frequency': self.hparams["eval_freq"] + 1
         }
         return [optimizer], [scheduler]
 
@@ -152,20 +156,6 @@ class TauScheduler(object):
     def __init__(self, start_value, factor, step_size):
         self.tau = start_value
         self.factor = factor
-        self.step_size = step_size
-        self.steps = 0
-
-    def __call__(self):
-        self.steps += 1
-        if self.steps >= self.step_size:
-            self.tau *= self.factor
-            self.steps = 0
-
-
-class GraphSizeScheduler(object):
-    def __init__(self, start_value, increment=1, step_size=1):
-        self.max_num_nodes = start_value
-        self.increment = increment
         self.step_size = step_size
         self.steps = 0
 
