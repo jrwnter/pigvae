905d712a7c8887ac27ca21e0fdb8ee307bb18962
jrwnter <robin.winter@bayer.com>
2021-01-13 16:21:51 +0100

    lr decay

--- graphae/trainer.py
+++ graphae/trainer.py
@@ -63,13 +63,10 @@ class PLGraphAE(pl.LightningModule):
         self.log_dict(metrics_soft)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.graph_ae.parameters(), lr=self.hparams["lr"])
-        lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer = torch.optim.Adam(self.graph_ae.parameters(), lr=self.hparams["lr"], betas=(0.9, 0.98))
+        lr_scheduler = torch.optim.lr_scheduler.LambdaLR(
             optimizer=optimizer,
-            factor=0.5,
-            patience=20,
-            cooldown=50,
-            min_lr=1e-6,
+            lr_lambda=lambda step: 0.03 * np.minimum(np.power(step, -0.5), step * np.power(4000, -1.5))
         )
         scheduler = {
             'scheduler': lr_scheduler,
@@ -78,15 +75,3 @@ class PLGraphAE(pl.LightningModule):
             'frequency': self.hparams["eval_freq"] + 1
         }
         return [optimizer], [scheduler]
-
-    def optimizer_step(self, epoch, batch_idx, optimizer, optimizer_idx, optimizer_closure=None,
-                       second_order_closure=None, on_tpu=False, using_native_amp=False, using_lbfgs=False):
-        # warm up lr
-        if self.trainer.global_step < 10000:
-            lr_scale = min(1., float(self.trainer.global_step + 1) / 10000.)
-            for pg in optimizer.param_groups:
-                pg['lr'] = lr_scale * self.hparams.lr
-
-        # update params
-        optimizer.step(closure=optimizer_closure)
-        optimizer.zero_grad()
