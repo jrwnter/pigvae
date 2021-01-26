5d7964ce5b0845fb9d67e83d055660852d6607ad
jrwnter <robin.winter@bayer.com>
2021-01-26 10:34:30 +0100

    minor

--- graphae/trainer.py
+++ graphae/trainer.py
@@ -11,9 +11,9 @@ class PLGraphAE(pl.LightningModule):
         self.graph_ae = GraphAE(hparams)
         self.critic = Critic(hparams["alpha"])
         self.tau_scheduler = TauScheduler(
-            start_value=1.0,
-            factor=0.98,
-            step_size=2
+            start_value=1,
+            factor=0.99,
+            step_size=5
         )
 
     def forward(self, graph, training, tau):
@@ -64,33 +64,34 @@ class PLGraphAE(pl.LightningModule):
         self.log_dict(metrics)
         self.log_dict(metrics_soft)
 
-    """def on_validation_epoch_end(self) -> None:
+        """def on_validation_epoch_end(self) -> None:
         self.tau_scheduler()"""
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.graph_ae.parameters(), lr=self.hparams["lr"], betas=(0.9, 0.98))
         lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(
             optimizer=optimizer,
-            gamma=0.98,
+            gamma=0.99,
         )
         scheduler = {
             'scheduler': lr_scheduler,
             'interval': 'step',
-            'frequency': self.hparams["eval_freq"] + 1
+            'frequency': 4 * (self.hparams["eval_freq"] + 1)
         }
         return [optimizer], [scheduler]
 
     def optimizer_step(self, epoch, batch_idx, optimizer, optimizer_idx, optimizer_closure=None,
                        second_order_closure=None, on_tpu=False, using_native_amp=False, using_lbfgs=False):
         # warm up lr
-        if self.trainer.global_step < 5000:
-            lr_scale = min(1., float(self.trainer.global_step + 1) / 5000.)
+        if self.trainer.global_step < 10000:
+            lr_scale = min(1., float(self.trainer.global_step + 1) / 10000.)
             for pg in optimizer.param_groups:
                 pg['lr'] = lr_scale * self.hparams.lr
 
         # update params
         optimizer.step(closure=optimizer_closure)
         optimizer.zero_grad()
+
 
 class TauScheduler(object):
     def __init__(self, start_value, factor, step_size):
